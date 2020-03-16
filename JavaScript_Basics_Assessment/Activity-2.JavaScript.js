239be38f8dff13be3823896b00618df3957776db
Loreena Moody <loreenadmoody@yahoo.com>
2020-03-15 20:33:43 -0500

    couldn't get it to run but I provided the answer

---
+++ JavaScript_Basics_Assessment/Activity-2.JavaScript.js
@@ -0,0 +1,26 @@
+console.log(9+9);
+    18
+
+console.log(56/12);
+  4.8  
+
+console.log(281/9);
+    31.2
+
+ console.log(18/0);
+    Infinity
+
+console.log("56"+"92")
+    5692
+
+console.log(42==52)
+    false
+
+
+
+
+
+
+
+
+
