da86908bff3c2a63bb627e91573f34a886f616f8
Loreena Moody <loreenadmoody@yahoo.com>
2020-03-16 00:49:15 -0500

    loreenam

---
+++ JavaScript_Basics_Assessment/Activity-4.JSAL.js
@@ -0,0 +1,9 @@
+studentList=["Joe","Kay","Jay"];
+var i=0; while(i<3){
+    console.log(i); i+++
+}
+var x=["Bill","Bob","Ann"];
+studentList. push("Bob","Bill","Ann")
+
+let count= 0; studentList.(function(element,i));
+{count +++;}; console.log(forEach processed;",name,elements");
