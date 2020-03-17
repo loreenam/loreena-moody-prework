6563b515043e94606e10a94458bb8977230c92cf
Loreena Moody <loreenadmoody@yahoo.com>
2020-03-17 11:23:37 -0500

    random words generator applied

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,21 +1,31 @@
 // Create an array of words
-var words = [
-    "April",
-    "May",
-    "June",
-    "birds",
-    "ranibow",
-    "kite",
-    "bunny",
-    "umbrella",
-    "baseball",
-    "sunshine",
-    "eggs",
-    "tulips",
-    "lamb",
-    "caterpillar",
-    "blooms",
-]
-// Pick a random word
-var word = words[Math.floor(Math.random() * words.length)];
+const word = [ 
+	"April",
+	"breeze",
+	"showers",
+	"birds",
+	"rainbow",
+	"kite",
+	"bunny",
+	"umbrella",
+	"baseball",
+	"sunshine",
+	"eggs",
+	"tulips",
+	"lamb",
+	"caterpillar",
+	"blooms",
+	];
 
+// Choose random word
+let randNum= Math.floor(Math.random() * word.length );
+let chosenWord= word[randNum];
+console.log(chosenWord);
+// Create underscores based on length of word
+// Get a guess from the user
+// Show the player progress
+// Update the game state with the guess
+// Check if guess is right
+// If right push to the right array
+// If wrong push to wrong array
+   
