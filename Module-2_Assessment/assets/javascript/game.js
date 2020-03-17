3eaac3c25907f2b3433007034db4e80bb9bde8bd
Loreena Moody <loreenadmoody@yahoo.com>
2020-03-17 06:30:54 -0500

    loreenam

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,2 +1,21 @@
 // Create an array of words
+var words = [
+    "April",
+    "May",
+    "June",
+    "birds",
+    "ranibow",
+    "kite",
+    "bunny",
+    "umbrella",
+    "baseball",
+    "sunshine",
+    "eggs",
+    "tulips",
+    "lamb",
+    "caterpillar",
+    "blooms",
+]
+// Pick a random word
+var word = words[Math.floor(Math.random() * words.length)];
 
